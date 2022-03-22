4bb7c64b3b7b7ecac0ecdbcc40a007afb59e9d24
Andrew Marshall <planetmarshalluk@gmail.com>
2022-03-22 21:42:29 +0000

    write tariff records to influx

--- octopus-influx.py
+++ octopus-influx.py
@@ -1,8 +1,8 @@
 import os
 from argparse import ArgumentParser
 import urllib.parse
-from pprint import pprint
 
+from influxdb_client import InfluxDBClient
 import pandas as pd
 import requests
 
@@ -20,14 +20,33 @@ def get_tariff(api_key, product_code="AGILE-OUTGOING-19-05-13", tariff_code="E-1
     end_point = f"/v1/products/{product_code}/electricity-tariffs/{tariff_code}/standard-unit-rates/"
     url = urllib.parse.urljoin(base_url, end_point)
     response = requests.get(url, auth=(api_key, None))
-    records = pd.DataFrame.from_records(response.json()["results"])
-    print(records)
+    records = response.json()
+    df = pd.DataFrame.from_records(records["results"])
+    df.set_index(pd.to_datetime(df.valid_from), inplace=True)
+    return df[["value_inc_vat"]] * 0.01
+
+
+def write_tariff_dataframe(tariff_dataframe: pd.DataFrame, influx_token=None):
+    with InfluxDBClient(url="http://localhost:8086", token=influx_token, org="home") as client:
+        with client.write_api() as write_client:
+            write_client.write(
+                "data",
+                "home",
+                record=tariff_dataframe,
+                data_frame_measurement_name="octopus_agile_outgoing"
+            )
 
 
 def main():
     api_key = os.environ["OCTOPUS_API_KEY"]
-    get_tariff(api_key, product_code="AGILE-OUTGOING-19-05-13", tariff_code="E-1R-AGILE-OUTGOING-19-05-13-G")
+    tariff_dataframe = get_tariff(
+        api_key,
+        product_code="AGILE-OUTGOING-19-05-13",
+        tariff_code="E-1R-AGILE-OUTGOING-19-05-13-G"
+    )
+    write_tariff_dataframe(tariff_dataframe)
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description="Get Octopus Outgoing Tariff and persist to InfluxDB")
     main()
